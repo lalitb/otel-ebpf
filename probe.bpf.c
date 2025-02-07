5ad9378559b2040e6f2d9b82d6a0da5974c8485e
Lalit Kumar Bhasin <labhas@microsoft.com>
2025-02-07 05:54:43 +0000

    Fix

--- probe.bpf.c
+++ probe.bpf.c
@@ -1,31 +1,39 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-#include <linux/ptrace.h>
 
-struct event_t {
-    __u64 timestamp_start;
-    __u64 timestamp_end;
+struct event {
+    __u64 duration_ns;
     __u32 pid;
     char comm[16];
 };
 
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u32);
+    __type(value, __u64);
+} start_times SEC(".maps");
+
 SEC("uprobe")
 int trace_enter(struct pt_regs *ctx) {
-    struct event_t event = {};
-    event.timestamp_start = bpf_ktime_get_ns();
-    event.pid = bpf_get_current_pid_tgid() >> 32;
-    bpf_get_current_comm(&event.comm, sizeof(event.comm));
-    bpf_printk("Enter: %s", event.comm);
+    __u32 pid = bpf_get_current_pid_tgid() >> 32;
+    __u64 ts = bpf_ktime_get_ns();
+    
+    bpf_map_update_elem(&start_times, &pid, &ts, BPF_ANY);
+    bpf_printk("Function entered: pid=%d", pid);
     return 0;
 }
 
 SEC("uretprobe")
 int trace_exit(struct pt_regs *ctx) {
-    struct event_t event = {};
-    event.timestamp_end = bpf_ktime_get_ns();
-    event.pid = bpf_get_current_pid_tgid() >> 32;
-    bpf_get_current_comm(&event.comm, sizeof(event.comm));
-    bpf_printk("Exit: %s", event.comm);
+    __u32 pid = bpf_get_current_pid_tgid() >> 32;
+    __u64 *start_ts = bpf_map_lookup_elem(&start_times, &pid);
+    
+    if (start_ts) {
+        __u64 duration = bpf_ktime_get_ns() - *start_ts;
+        bpf_printk("Function exited: pid=%d duration=%llu ns", pid, duration);
+        bpf_map_delete_elem(&start_times, &pid);
+    }
     return 0;
 }
 
